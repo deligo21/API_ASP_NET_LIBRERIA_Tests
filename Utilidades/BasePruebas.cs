35fadcac5d040bfbc651bfef4f70db82e0a4740f
arrp1239@gmail.com <arrp1239@gmail.com>
2025-08-14 22:23:46 -0400

    configuracion de nuevas pruebas

---
+++ Utilidades/BasePruebas.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BibliotecaAPI.Datos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPITest.Utilidades
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreBD)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreBD)
+                .Options;
+            var dbCcontext = new ApplicationDbContext(opciones);
+            return dbCcontext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var configExpression = new MapperConfigurationExpression();
+            configExpression.AddProfile(new BibliotecaAPI.Utilidades.AutoMapperProfiles());
+
+            var configuration = new MapperConfiguration(configExpression, loggerFactory: null);
+
+            return configuration.CreateMapper();
+        }
+    }
+}
