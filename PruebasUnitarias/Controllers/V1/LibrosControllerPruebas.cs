516839956cbb114f02c58647364c979c5c03cff2
arrp1239@gmail.com <arrp1239@gmail.com>
2025-08-20 21:16:45 -0400

    pruebas con HttpContext y Claims

---
+++ PruebasUnitarias/Controllers/V1/LibrosControllerPruebas.cs
@@ -0,0 +1,35 @@
+﻿using BibliotecaAPITest.Utilidades;
+using Microsoft.AspNetCore.OutputCaching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPITest.PruebasUnitarias.Controllers.V1
+{
+    [TestClass]
+    public class LibrosControllerPruebas: BasePruebas
+    {
+        [TestMethod]
+        public async Task Get_RetornarCeroLibros_CuandoNoHayLibros()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+            IOutputCacheStore outputCacheStore = null!;
+
+            var controller = new BibliotecaAPI.Controllers.v1.LibrosController(contexto, mapper, null!, outputCacheStore);
+            controller.ControllerContext.HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+            var paginacionDTO = new BibliotecaAPI.DTOs.PaginacionDTO(1, 1);
+
+            // Prueba
+
+            var respuesta = await controller.Get(paginacionDTO);
+
+            // Verificación
+            Assert.AreEqual(expected: 0, actual: respuesta.Count());
+        }
+    }
+}
---
+++ PruebasUnitarias/Servicios/ServiciosUsuariosPruebas.cs
@@ -0,0 +1,94 @@
+﻿using BibliotecaAPI.Entidades;
+using BibliotecaAPI.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPITest.PruebasUnitarias.Servicios
+{
+    [TestClass]
+    public class ServiciosUsuariosPruebas
+    {
+        private UserManager<Usuario> userManager = null!;
+        private IHttpContextAccessor contextAccessor = null!;
+        private ServicioUsuarios servicioUsuarios = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            userManager = Substitute.For<UserManager<Usuario>>(
+                Substitute.For<IUserStore<Usuario>>(), null, null, null, null, null, null, null, null);
+            contextAccessor = Substitute.For<Microsoft.AspNetCore.Http.IHttpContextAccessor>();
+            servicioUsuarios = new BibliotecaAPI.Servicios.ServicioUsuarios(userManager, contextAccessor);
+        }
+
+        [TestMethod]
+        public async Task ObtenerUsuario_RetornarNulo_CuandoNoHayClaimEmail()
+        {
+            // Preparación
+            var httpContext = new DefaultHttpContext();
+            contextAccessor.HttpContext.Returns(httpContext);
+
+            // Prueba
+            var usuario = await servicioUsuarios.ObtenerUsuario();
+
+            // Verificación
+            Assert.IsNull(usuario);
+        }
+
+        [TestMethod]
+        public async Task ObtenerUsuario_RetornarUsuario_CuandoHayClaimEmail()
+        {
+            // Preparación
+            var email = "prueba@gmail.com";
+            var usuarioEsperado = new Usuario { Email = email };
+
+            userManager.FindByEmailAsync(email)!.Returns(Task.FromResult(usuarioEsperado));
+
+            var claims = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", email)
+            }));
+
+            var httpContext = new DefaultHttpContext() { User = claims};
+            contextAccessor.HttpContext.Returns(httpContext);
+
+            // Prueba
+            var usuario = await servicioUsuarios.ObtenerUsuario();
+
+            // Verificación
+            Assert.IsNotNull(usuario);
+            Assert.AreEqual(email, usuario.Email);
+        }
+
+        [TestMethod]
+        public async Task ObtenerUsuario_RetornarNulo_CuandoUsuarioNoExiste()
+        {
+            // Preparación
+            var email = "prueba@gmail.com";
+            var usuarioEsperado = new Usuario { Email = email };
+
+            userManager.FindByEmailAsync(email)!.Returns(Task.FromResult<Usuario>(null!));
+
+            var claims = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", email)
+            }));
+
+            var httpContext = new DefaultHttpContext() { User = claims};
+            contextAccessor.HttpContext.Returns(httpContext);
+
+            // Prueba
+            var usuario = await servicioUsuarios.ObtenerUsuario();
+
+            // Verificación
+            Assert.IsNull(usuario);
+        }
+    }
+}
